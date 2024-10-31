2bd8b20899f82ed74d2c5c42e48ee489cc7dccad
sebastianmr18 <juan.munoz.rojas@correounivalle.edu.co>
2024-10-31 16:34:29 -0500

    refactor/add serializers and views from backend

--- server/app/urls.py
+++ server/app/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path, include
 from rest_framework.documentation import include_docs_urls
 from rest_framework.routers import DefaultRouter
-from .views.views import  CourtViewSet, ReservationViewSet, PaymentViewSet, ReviewViewSet
+from .views.views import  CourtViewSet, ReservationViewSet, PaymentViewSet, ReviewViewSet, CouponViewSet, NotificationViewSet, ReservationHistoryViewSet
 from .views.user_view import UserViewSet,UserRegistrationView
 
 router = DefaultRouter()
@@ -10,6 +10,9 @@ router.register(r'courts', CourtViewSet)
 router.register(r'reservations', ReservationViewSet)
 router.register(r'payments', PaymentViewSet)
 router.register(r'reviews', ReviewViewSet)
+router.register(r'coupons', CouponViewSet)
+router.register(r'notifications', NotificationViewSet)
+router.register(r'reservation_history', ReservationHistoryViewSet)
 
 urlpatterns = [
     path('docs/', include_docs_urls(title='API documentation')),
